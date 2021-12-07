ec54d132f5d81f393699efc8e49e2976936a92f2
wendy223 <30325291+wendy223@users.noreply.github.com>
2021-12-06 22:37:02 -0500

    Update hm_convert.py

--- mmf_cli/hm_convert.py
+++ mmf_cli/hm_convert.py
@@ -62,7 +62,7 @@ class HMConverter:
         exists = False
 
         for file in files_needed:
-            exists = exists or PathManager.exists(os.path.join(folder, "data", file))
+            exists = exists or PathManager.exists(os.path.join(folder, "hateful_memes", file))
 
         if not exists:
             raise AssertionError("Neither img or img.tar.gz exists in current zip")
